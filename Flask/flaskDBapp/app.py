c68b9c81ddb57955d9384590207e562514e42844
CharlieRosander <112691863+CharlieRosander@users.noreply.github.com>
2023-03-17 15:59:03 +0100

    df

---
+++ Flask/flaskDBapp/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+def create_tables():
+    db.create_all()
+
+with app.app_context():
+    create_tables()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    email = request.form['email']
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('users'))
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', users=all_users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Flask/kaliberPage/app.py
+++ Flask/kaliberPage/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from werkzeug.security import check_password_hash, generate_password_hash
 import requests
 import openai
 import os
@@ -12,6 +14,7 @@ current_time = datetime.datetime.now()
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 openweathermap_api_key = os.getenv('OPENWEATHERMAP_API_KEY')
+app.secret_key = 'my-secret-key'
 
 weather_images = {
     'clear sky': 'https://openweathermap.org/img/wn/01d@2x.png',
@@ -24,7 +27,42 @@ weather_images = {
     'snow': 'https://openweathermap.org/img/wn/13d@2x.png',
 }
 
+
 @app.route("/")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print('login func called')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        print(f'username: {username}, password: {password}')
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash('Login successful', 'success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
+
+@app.route("/signup")
+def sign_up():
+    return render_template('signup.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
+
 @app.route("/home")
 def home_page():
     city_query = request.args.to_dict()
@@ -44,13 +82,15 @@ def home_page():
         feels_like = response['main']['feels_like']
         image = weather_images.get(description, None)
     except (KeyError, IndexError):
-        return render_template('error.html')
+        return render_template('home.html')
 
     return render_template('home.html', location=location, temperature=temperature, description=description, feels_like=feels_like, image=image)
+
 
 @app.route('/chatgpt')
 def chatgpt_page():
     return render_template("chatgpt.html")
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -67,13 +107,45 @@ def chat():
     gpt_response = response.choices[0].text.strip()
     return str(gpt_response)
 
+
 @app.route("/about")
 def about_page():
     return render_template("about.html")
 
+
 @app.route('/contact')
 def contact_page():
     return render_template("contact.html")
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'hejhopp'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+def create_tables():
+    db.create_all()
+
+
+with app.app_context():
+    create_tables()
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', users=all_users)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
