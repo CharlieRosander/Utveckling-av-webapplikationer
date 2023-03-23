f95f4e2ee045bea15cb778112188a50a9e890a59
CharlieRosander <112691863+CharlieRosander@users.noreply.github.com>
2023-03-23 20:00:39 +0100

    asd
    asd

--- Examinering/Pizzeria/app.py
+++ Examinering/Pizzeria/app.py
@@ -1,28 +1,45 @@
 from flask import Flask, render_template, redirect, url_for, session, flash, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
 
+
+    #### READ CSV FILE ####
+def read_csv():
+    csv = ("./Docs/pizzeria.csv")
+    df = pd.read_csv(csv)
+    return print(df)
+
+with app.app_context():
+    read_csv()
+    #### /READ CSV FILE ####
+
+
+#### DATABASE ####
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#### DATABASE ####
-class User(db.Model):
+class Admin(db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+
 
 def create_admin():
-    admin = User.query.filter_by(username='admin').first()
+    admin = Admin.query.filter_by(username='admin').first()
     if admin is None:
-        admin = User(username='admin', password='123')
+        admin = Admin(username='admin', password='123')
         db.session.add(admin)
         db.session.commit()
 
@@ -34,34 +51,74 @@ with app.app_context():
     create_admin()
 #### /DATABASE ####
 
+#### REGISTER ####
+@app.route("/signup")
+def sign_up():
+    return render_template('signup.html')
+
+# Add user to database #
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+#### /REGISTER ####
+
 #### LOGIN ####
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    current_user = None
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        username = request.form['username']
+        password = request.form['password']
+        admin = Admin.query.filter_by(username=username).first()
         user = User.query.filter_by(username=username).first()
+        
+
+        if admin:
+            if admin.password == password:
+                session["user"] = admin.username
+                return redirect(url_for('index'))
+
         if user:
-            if user.username == username and user.password == password:
-                session['user_id'] = user.id
-                current_user = User.query.filter_by(id=session['user_id']).first()
-                flash('Logged in successfully.')
+            if check_password_hash(user.password, password):
+                session["user"] = user.username
                 return redirect(url_for('index'))
             else:
-                error = ('Incorrect password, try again.')
+                error = 'Incorrect password, try again.'
         else:
-            error = ('Username does not exist.')
-    return render_template('login.html', error=error, current_user=current_user)
+            error = 'Username does not exist.'
+
+    return render_template('login.html', error=error)
 #### /LOGIN ####
 
+#### LOGOUT ####
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('Logged out successfully.')
+    return redirect(url_for('login'))
+#### /LOGOUT ####
+
+#### PAGE ROUTES ####
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+#### /PAGE ROUTES ####
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
